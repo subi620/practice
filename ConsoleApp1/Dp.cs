d102a7257003ac75529224d28b94b83790362d8d
subi620 <subi620@naver.com>
2023-01-31 17:56:46 +0900

    상속

---
+++ ConsoleApp1/Dp.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Dp
+    {
+        static void Main(string[] args)
+        {
+            Parentt parentt = new Parentt();
+            parentt.A();
+
+            Childd childd = new Childd();
+            childd.A();
+
+            Daughter daughter = new Daughter();
+            daughter.A();
+        }
+    }
+
+    class Parentt
+    {
+        public virtual void A()
+        {
+            Console.WriteLine("부모 클래스의 A() 메서드 호출!");
+        }
+    }
+
+    class Childd : Parentt
+    {
+        public override void A()
+        {
+            Console.WriteLine("자식 클래스(Child)의 A() 메서드 호출!");
+        }
+    }
+
+    class Daughter : Parentt
+    {
+        public override void A()
+        {
+            Console.WriteLine("자식 클래스(Daughter)의 A() 메서드 호출!");
+        }
+    }
+
+    
+}
---
+++ ConsoleApp1/MyMy.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class MyMy
+    {
+        static void Main_ddd(string[] args)
+        {
+            MyClasss mc = new MyClasss();
+            Console.WriteLine("mc.Name: {0}", mc.Name);
+
+            mc.Name = "Kelley";
+            Console.WriteLine("mc.Name:{0}", mc.Name);
+        }
+    }
+
+    public class MyClasss
+    {
+        private string name = "John"; // private 제한
+
+        public string Name
+        {
+            get
+            {
+                return name; // 반환값
+            }
+            set
+            {
+                if(value.Length < 5) // value의 길이가 5보다 작아야 새 값 할당
+                    name = value; 
+            }
+        }
+    }
+}
