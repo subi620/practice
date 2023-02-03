47f9bbdb2fb7b28967c61a23e3beaca1cde3b421
subi620 <subi620@naver.com>
2023-02-03 15:33:46 +0900

    분할, 중첩 메소드

---
+++ ConsoleApp1/Nested.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    partial class Nested
+    {
+        public void Test()
+        {
+            Console.WriteLine("Test()");
+        }
+    }
+
+    partial class Nested
+    {
+        public void Test2()
+        {
+            Console.WriteLine("Test2()");
+        }
+    }
+
+    partial class Nested
+    {
+        public void Test3()
+        {
+            Console.WriteLine("Test3()");
+        }
+    }
+
+    class Program31
+    {
+        static void Main_12(string[] args)
+        {
+            Nested nested = new Nested();
+            nested.Test();
+            nested.Test2();
+            nested.Test3();
+        }
+    }
+}
---
+++ ConsoleApp1/Outerclass.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class OuterClass
+    {
+        private int a = 70;
+
+        public class InnerClass
+        {
+            OuterClass instance;
+
+            public InnerClass(OuterClass a_instance)
+            {
+                instance = a_instance;
+            }
+
+            public void AccessVariable(int num)
+            {
+                this.instance.a = num;
+            }
+
+            public void ShowVariable()
+            {
+                Console.WriteLine("a:{0}", this.instance.a);
+            }
+        }
+        class Program33
+        {
+            static void Main(string[] args)
+            {
+                OuterClass outer = new OuterClass();
+                OuterClass.InnerClass inner = new OuterClass.InnerClass(outer);
+
+                inner.ShowVariable();
+                inner.AccessVariable(60);
+                inner.ShowVariable();
+
+            }
+        }
+
+       
+    }
+}
